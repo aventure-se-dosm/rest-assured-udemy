82f49c325223465d3c0f634c08d94dd90c5279f5
Marcelo de Oliveira Santos <=>
2023-08-04 13:53:24 -0300

    26th lession - empty field error

---
+++ src/main/java/br/dev/marcelodeoliveira/rest/VerbosTest.java
@@ -0,0 +1,99 @@
+package br.dev.marcelodeoliveira.rest;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+import org.apache.http.HttpStatus;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+
+import io.restassured.http.ContentType;
+import io.restassured.http.Method;
+
+
+public class VerbosTest {
+	
+	private final  String URL_BASE = "https://restapi.wcaquino.me";
+	private final  String URL_RESOURCE_USERS = "/users";
+	
+	private String getUsersResource() {
+		return this.URL_RESOURCE_USERS;
+	}
+	
+	private String getUrlBase() {
+		return this.URL_BASE;
+	}
+	
+	private String getUsersEndpoint() {
+		return getUrlBase() + getUsersResource();
+	}
+	
+	private void someSwitchByVerb(Method method) {
+		//TODO: any method which some arg is supposded to be a REST verb enum.
+		// look at this as a template.
+	}
+	
+	//na verdade precisamos gerar uma DTO
+	//na arquitetura maior
+	private String getUserJson(String name, Integer age) {
+
+		//it works: oneline simplest Hard-coded json
+		// String body =  String.format("{\"name\":\"%s\",\"age\":%d}", name, age);
+		//String body =  String.format("{\"name\":\"%s\",\"age\":%s}", name, age.toString()); 
+		
+		 String	body =  String.format("{\n"
+				+ "\t" + "\"name\":\t\"%s\"\n"+","
+				+ "\t" + "\"age\"\t:%d\n"
+				+ "}",
+				name,age);
+		//It also works well:
+		return body;
+	}
+	
+	@Test
+	public void deveSalvarUmUsuario() {
+		given().log().all()
+			.contentType(ContentType.JSON)
+			.body(getUserJson("José", 50))
+		.when()
+			.post(getUsersEndpoint())
+		.then().log().all().assertThat()
+			.body("id", Matchers.is(notNullValue()))
+			.body("name", is("José"))
+			.body("age", greaterThan(new Integer(0)))
+			.body("age", is(50))
+			
+		;
+	}
+	
+	@Test
+	public void naoDeveSalvarUmUsuarioNomeStrVazia() {
+		given().log().all()
+			.contentType(ContentType.JSON)
+			.body(getUserJson("", 50)) // it works: null is interpreted as if the String "null";
+		.when()
+			.post(getUsersEndpoint())
+		.then().log().all()
+			.assertThat()
+			.statusCode(HttpStatus.SC_BAD_REQUEST)
+			.body("id", Matchers.is(nullValue()))
+			.body("error", is("Name é um atributo obrigatório"))			
+		;
+	}
+	
+	@Test
+	public void naoDeveSalvarUmUsuarioSimNome() {
+		given().log().all()
+			.contentType(ContentType.JSON)
+			.body("{\"age\": 50}") //it fails, no setup for attribute "name".
+		.when()
+			.post(getUsersEndpoint())
+		.then().log().all()
+			.assertThat()
+			.statusCode(HttpStatus.SC_BAD_REQUEST)
+			.body("id", Matchers.is(nullValue()))
+			.body("error", is("Name é um atributo obrigatório"))		
+		;
+	}
+
+}
