05ae9d18e5c776732a55b265f1bfbf0b9b09ab5e
Marcelo de Oliveira Santos <=>
2023-08-08 15:11:30 -0300

    40th lession: xpath with html

--- src/main/java/br/dev/marcelodeoliveira/rest/HTML.java
+++ src/main/java/br/dev/marcelodeoliveira/rest/HTML.java
@@ -15,7 +15,7 @@ public class HTML {
 	public void  deveFazerBuscaComHtml() {
 		given().log().all()
 		.when()
-			.get("https://restapi.wcaquino.me/v2/users")
+		.get("https://restapi.wcaquino.me/v2/users")
 		.then().log().all()
 			.contentType(ContentType.HTML)
 			.statusCode(HttpStatus.SC_OK)
@@ -39,6 +39,70 @@ public class HTML {
 			.body("html.body.div.table.tbody.tr[2].td[1]", is("Ana Júlia"))
 			.body("html.body.div.table.tbody.tr[2].td[2]", is("20"))
 			
+//			.body("html.body.div.table.tbody.tr[1].td[2]", is(30)) //it doesn't work: String value 
+		;
+		
+	}
+	
+	@Test
+	public void  deveFazerBuscaComHtmlEXpath() {
+		given().log().all()
+		.when()
+		.then().log().all()
+		.contentType(ContentType.HTML)
+		.statusCode(HttpStatus.SC_OK)
+		
+		// indexation here is starts with 0 ending in n-1, for n > 0 elements for row or colum
+		
+		.body(hasXPath("count(//table//theader)", is("1")))
+		.body(hasXPath("count(//table//tr)", is("4")))
+		.body("html.body.div.table.thead.tr.th[1]", is("Nome"))
+		.body("html.body.div.table.thead.tr.th[2]", is("Idade"))
+//		
+		.body(hasXPath("//tr[1]/td[1]", is("1")))
+		.body(hasXPath("//tr[1]/td[2]", is("João da Silva")))
+		.body(hasXPath("//tr[1]/td[3]", is("30")))
+		
+		.body(hasXPath("//tr[2]/td[1]", is("2")))
+		.body(hasXPath("//tr[2]/td[2]", is("Maria Joaquina")))
+		.body(hasXPath("//tr[2]/td[3]", is("25")))
+		
+		.body(hasXPath("//tr[3]/td[1]", is("3")))
+		.body(hasXPath("//tr[3]/td[2]", is("Ana Júlia")))
+		.body(hasXPath("//tr[3]/td[3]", is("20")))
+		
+//			.body("html.body.div.table.tbody.tr[1].td[2]", is(30)) //it doesn't work: String value 
+		;
+	}
+	
+	@Test
+	public void  deveFazerBuscaComHtmlEXpathClean() {
+		given().log().all()
+		.queryParams("format", "clean")
+		.when()
+		.then().log().all()
+		.contentType(ContentType.HTML)
+		.statusCode(HttpStatus.SC_OK)
+		
+		// indexation here is starts with 0 ending in n-1, for n > 0 elements for row or colum
+		
+		//.body(hasXPath("count(//table//theader)", is("1")))
+		//.body(hasXPath("count(//table//tr)", is("4")))
+//		.body("html.body.div.table.thead.tr.th[1]", is("Nome"))
+//		.body("html.body.div.table.thead.tr.th[2]", is("Idade"))
+//		
+		.body(hasXPath("//tr[2]/td[1]", is("1")))
+		.body(hasXPath("//tr[2]/td[2]", is("João da Silva")))
+		.body(hasXPath("//tr[2]/td[3]", is("30")))
+		
+		.body(hasXPath("//tr[3]/td[1]", is("2")))
+		.body(hasXPath("//tr[3]/td[2]", is("Maria Joaquina")))
+		.body(hasXPath("//tr[3]/td[3]", is("25")))
+		
+		.body(hasXPath("//tr[4]/td[1]", is("3")))
+		.body(hasXPath("//tr[4]/td[2]", is("Ana Júlia")))
+		.body(hasXPath("//tr[4]/td[3]", is("20")))
+		
 //			.body("html.body.div.table.tbody.tr[1].td[2]", is(30)) //it doesn't work: String value 
 		;
 		
