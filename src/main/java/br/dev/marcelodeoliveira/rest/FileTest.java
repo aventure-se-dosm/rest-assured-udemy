f0f8c334e2b5aa1b0fc1688cd1715c43ccc08c73
Marcelo de Oliveira Santos <=>
2023-08-09 10:07:42 -0300

    42nd lesson - File Opload with timout and exceding file size limit issues

--- src/main/java/br/dev/marcelodeoliveira/rest/FileTest.java
+++ src/main/java/br/dev/marcelodeoliveira/rest/FileTest.java
@@ -1,18 +1,18 @@
 package br.dev.marcelodeoliveira.rest;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.lessThan;
 
 import java.io.File;
 
 import org.apache.http.HttpStatus;
 import org.junit.Test;
 
-import io.restassured.http.ContentType;
-
 public class FileTest {
 
 	private final String UrlFileUpload = "https://restapi.wcaquino.me/upload";
 	private final String PathUploadedFile = "src/main/resources/users.pdf";
+	private final String PathTooMuchLargeUploadedFile = "src/main/resources/chromedriver_win32_111.zip";
 
 	@Test
 	public void deveObrigarEnvioArquivo() {
@@ -26,16 +26,37 @@ public class FileTest {
 
 	@Test
 	public void deveFazerEeArquivo() {
-		given().log().all()
-			//.contentType("multipart/form-data") --> optional
+		//.contentType("multipart/form-data") --> optional
 		
-			//	'Content-type' has an attribute called name.
-			//	"arquivo" is set for this endpoint, standing for files to upload
-			// which Content-type is 'multipart/form-data'
+		//	'Content-type' has an attribute called name.
+		//	"arquivo" is set for this endpoint, standing for files to upload
+		// which Content-type is 'multipart/form-data'
+		given().log().all()
 			.multiPart("arquivo", new File(PathUploadedFile))
 		.when()
 			.post(UrlFileUpload)
 		.then().log().all()
 				.statusCode(HttpStatus.SC_OK);
+	}
+	
+	@Test
+	public void naoDeveFazerUploadDeArquivoMuitoGrande() {
+		given().log().all()
+			.multiPart("arquivo", new File(PathTooMuchLargeUploadedFile))
+		.when()
+			.post(UrlFileUpload)
+		.then().log().all()
+			.statusCode(HttpStatus.SC_REQUEST_TOO_LONG);
+	}
+
+	@Test
+	public void naoDeveFazerUploadDeArquivoTimeoutException() {
+		given().log().all()
+		.multiPart("arquivo", new File(PathTooMuchLargeUploadedFile))
+		.when()
+		.post(UrlFileUpload)
+		.then().log().all()
+		.time(lessThan(10000L))
+		.statusCode(HttpStatus.SC_REQUEST_TOO_LONG);
 	}
 }
