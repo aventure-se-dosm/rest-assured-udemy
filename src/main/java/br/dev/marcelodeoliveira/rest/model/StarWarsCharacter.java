a31d3f9c6ea6127cfff58bb42b948acb6c319f37
Marcelo de Oliveira Santos <=>
2023-08-11 10:30:14 -0300

    48th lesson - API testing with API key for requests

---
+++ src/main/java/br/dev/marcelodeoliveira/rest/model/StarWarsCharacter.java
@@ -0,0 +1,20 @@
+package br.dev.marcelodeoliveira.rest.model;
+
+import java.util.List;
+
+public class StarWarsCharacter {
+	private String name;
+	private Integer height;
+	private Integer mass;
+	private String hairColor;
+	private String skinColor;
+	private Integer yearOfBirth;
+	private String gender;
+	private String homeworld;
+	private List<String> films;
+	private List<String> species;
+	private List<String> vehicles;
+	private String created;
+	private String edited;
+	private String url;
+}
